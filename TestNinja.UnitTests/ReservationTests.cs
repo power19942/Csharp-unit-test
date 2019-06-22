3176ef86762731a2a1c16f69d70ca1e02a8a9f6a
omar jakmira <poweromar19942@gmail.com>
2019-06-22 12:20:24 -0800

    Add project files.

---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //arrance
+            var reservition = new Reservation();
+            //act
+            var res = reservition.CanBeCancelledBy(new User { IsAdmin = true});
+            //assert
+            Assert.AreEqual(res, true);
+        }
+    }
+}
